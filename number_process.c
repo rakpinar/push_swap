5dc61eebd0f9ba28c7776d1cf6b5c36b54595120
Rumeysa Nur Akpinar <rakpinar@k1m41s06.42istanbul.com.tr>
2023-07-10 20:04:37 +0300

    Inital commit

---
+++ number_process.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   number_process.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rakpinar <rakpinar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/10 19:54:10 by rakpinar          #+#    #+#             */
+/*   Updated: 2023/07/10 19:54:13 by rakpinar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_numprocess(t_data *p, int ac, char **av)
+{
+	int	i;
+	int	j;
+	int	nm_start;
+
+	ft_init(p);
+	i = 0;
+	while (++i < ac)
+	{
+		j = 0;
+		nm_start = 0;
+		while (av[i][j])
+		{
+			while (av[i][j] && av[i][j] == ' ')
+				j++;
+			nm_start = j;
+			if (av[i][j] == '+' || av[i][j] == '-')
+				j++;
+			while (av[i][j] && av[i][j] != ' ')
+				j++;
+			ft_putnumber(p, &av[i][nm_start]);
+			while (av[i][j] && av[i][j] == ' ')
+				j++;
+		}
+	}
+}
+
+void	ft_putnumber(t_data *p, char *str)
+{
+	int	number;
+
+	number = ft_atoi(str);
+	if (ft_check_samenum(p->arr_a, number,
+			p->total_size - p->a_size, p->total_size) == 1)
+		ft_error("Error");
+	ft_array_push(p->arr_a, (p->total_size - 1) - p->a_size++, number);
+}
---
+++ utils_str.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_str.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rakpinar <rakpinar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/10 19:55:52 by rakpinar          #+#    #+#             */
+/*   Updated: 2023/07/10 19:56:00 by rakpinar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	ft_putstr(char *str)
+{
+	int	len;
+
+	len = ft_strlen(str);
+	return (write(1, str, len));
+}
